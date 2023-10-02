ab9cfea8fcb107851177604ff78774a00cf521c8
DevanshSK <devanshs843@gmail.com>
2023-10-02 23:14:36 +0530

    Frontend Setup completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sih-book-review-portal-1340
+
 This is the solution repository of the problem statement 1340 of Smart India Hackathon 2023
+Hey There
---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is home page</h1>
+      <Button>Click ME</Button>
+    </div>
+  );
+}
